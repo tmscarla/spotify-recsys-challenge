bacfb0b6e9744161f589cc0722cfbb5d556c84fc
Jessica <kangshuwen26@126.com>
2018-07-05 21:12:00 +0200

    Update top_pop_album.py

--- recommenders/script/creative/top_pop_album.py
+++ recommenders/script/creative/top_pop_album.py
@@ -1,12 +1,12 @@
 import sys
-from scripts.top_pop_p import Top_pop_p
+from utils.definitions import ROOT_DIR
+from recommenders.script.main.top_pop_p import Top_pop_p
 import scipy.sparse as sps
 
 arg = sys.argv[1:]
 mode = arg[0]
 
 
-if mode == "online":
-    t = Top_pop_p()
-    eurm = t.get_top_pop_album()
-    sps.save_npz("top_pop_2_album_"+mode+".npz", eurm)
+t = Top_pop_p()
+eurm = t.get_top_pop_album(mode)
+sps.save_npz(ROOT_DIR+"/recommenders/script/creative/"+mode+"_npz/top_pop_2_album_"+mode+".npz", eurm)
